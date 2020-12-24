1b84af1784795e6659d1d30ea8921ec3acdc4466
danielVFS <danielv759153@gmail.com>
2020-12-24 18:16:43 -0300

    add: expo-google-fonts/roboto on theme

--- src/screens/Loading/index.js
+++ src/screens/Loading/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { View, Text } from "react-native";
 
+import { FONTS, COLORS } from "../../themes/theme";
+
 export default function Loading() {
   return (
-    <View>
-      <Text>Loading</Text>
+    <View style={{ marginTop: 50 }}>
+      <Text style={{ fontSize: 50, ...FONTS.h1 }}>Loading</Text>
     </View>
   );
 }
