ee68fae24fff98b2df649bdb74f8f12051c38b30
danielVFS <danielv759153@gmail.com>
2020-12-30 11:30:25 -0300

    feat: credit-card input

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -4,19 +4,22 @@ import {
   SafeAreaView,
   Text,
   StyleSheet,
-  Image,
+  UIManager,
   TextInput,
+  ScrollView,
   TouchableOpacity,
   Dimensions,
 } from "react-native";
-
 import * as Animatable from "react-native-animatable";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import Feather from "react-native-vector-icons/Feather";
+import { CreditCardInput } from "react-native-credit-card-input";
 
 import { COLORS, FONTS, SIZES } from "../../themes/theme";
 
 import { AuthContext } from "../../hooks/authContext";
+
+UIManager.setLayoutAnimationEnabledExperimental(true);
 
 export default function SignIn({ navigation }) {
   const { signUp } = React.useContext(AuthContext);
@@ -92,14 +95,30 @@ export default function SignIn({ navigation }) {
     });
   };
 
+  const _onChange = (form) => console.log(form);
+
+  const _onFocus = (field) => console.log("focusing", field);
+
   return (
     <View style={styles.container}>
       <SafeAreaView style={styles.header}></SafeAreaView>
-      <Animatable.View animation="fadeInUpBig" style={styles.footer}>
+      <ScrollView style={styles.footer} showsVerticalScrollIndicator={false}>
         <View style={styles.titleArea}>
           <Text style={styles.titleText}>Registre-se</Text>
+          <View style={styles.accountRegister}>
+            <Text style={{ fontSize: SIZES.body4, color: COLORS.darkgray }}>
+              Já tem uma conta ?
+            </Text>
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              style={styles.signUpButton}
+            >
+              <Text style={styles.signUpButtonText}>Voltar</Text>
+            </TouchableOpacity>
+          </View>
         </View>
-        <View style={[styles.action, { marginTop: SIZES.marginTop }]}>
+
+        <View style={[styles.action]}>
           <FontAwesome name="male" color={COLORS.primary} size={25} />
           <TextInput
             placeholder="Nome Completo"
@@ -145,6 +164,7 @@ export default function SignIn({ navigation }) {
             )}
           </TouchableOpacity>
         </View>
+
         <View style={[styles.action, { marginTop: SIZES.marginTop }]}>
           <FontAwesome name="lock" color={COLORS.primary} size={25} />
           <TextInput
@@ -162,6 +182,33 @@ export default function SignIn({ navigation }) {
           </TouchableOpacity>
         </View>
 
+        <View style={[styles.action, { marginTop: SIZES.marginTop }]}>
+          <CreditCardInput
+            requiresName={true}
+            requiresCVC={true}
+            validColor={COLORS.darkGreen}
+            invalidColor={COLORS.red}
+            placeholderColor="darkgray"
+            allowScroll
+            placeholders={{
+              number: "5386 9596 2508 0429",
+              expiry: "08/21",
+              cvc: "416",
+              name: "JOSÉ DA SILVA",
+            }}
+            labels={{
+              number: "NÚMERO DO CARTÃO",
+              expiry: "VALIDO ATÉ",
+              cvc: "CVV/CVC",
+              name: "NOME COMPLETO",
+            }}
+            labelStyle={{ color: "black", fontSize: 12 }}
+            inputStyle={{ color: "black", fontSize: 16 }}
+            onChange={_onChange}
+            onFocus={_onFocus}
+          />
+        </View>
+
         <View style={styles.footerSection}>
           <TouchableOpacity
             style={styles.signInButton}
@@ -171,20 +218,8 @@ export default function SignIn({ navigation }) {
               Registrar-se
             </Text>
           </TouchableOpacity>
-
-          <View style={styles.accountRegister}>
-            <Text style={{ fontSize: SIZES.body4, color: COLORS.darkgray }}>
-              Já tem uma conta ?
-            </Text>
-            <TouchableOpacity
-              onPress={() => navigation.goBack()}
-              style={styles.signUpButton}
-            >
-              <Text style={styles.signUpButtonText}>Voltar</Text>
-            </TouchableOpacity>
-          </View>
         </View>
-      </Animatable.View>
+      </ScrollView>
     </View>
   );
 }
@@ -207,6 +242,7 @@ const styles = StyleSheet.create({
     paddingVertical: 20,
     borderTopLeftRadius: 30,
     borderTopRightRadius: 30,
+    marginBottom: 0,
   },
   text_footer: {
     color: COLORS.primary,
@@ -238,6 +274,7 @@ const styles = StyleSheet.create({
   footerSection: {
     alignItems: "center",
     marginTop: 50,
+    marginBottom: 50,
   },
   signInButton: {
     width: "100%",
@@ -260,7 +297,6 @@ const styles = StyleSheet.create({
   },
   accountRegister: {
     flexDirection: "row",
-    marginTop: 20,
   },
   signUpButton: {
     paddingLeft: 6,
